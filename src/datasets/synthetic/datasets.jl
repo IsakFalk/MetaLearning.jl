d0340b2c90ff440c834550f15985b337e90185a1
Isak Falk <me@isakfalk.com>
2020-01-29 15:26:20 +0000

    Add SinusoidLinearmixture and reorganise dirs

--- src/datasets/synthetic/datasets.jl
+++ src/datasets/synthetic/datasets.jl
@@ -10,7 +10,6 @@ struct Sinusoid <: AbstractSyntheticDataSet
     p_ϕ::UnivariateDistribution
     p_x::UnivariateDistribution
 end
-
 Sinusoid() = Sinusoid(Uniform(0.1, 0.5), Uniform(0.0, π), Uniform(-5.0, 5.0))
 
 function Base.rand(p::Sinusoid, k::Int)
@@ -29,7 +28,55 @@ function Base.rand(p::Sinusoid)
 end
 
 # PMAML Sinusoid+Line
+struct SinusoidLinearMixture <: AbstractSyntheticDataSet
+    p_sinusoid::Bernoulli
+    # Sinusoid
+    p_A::UnivariateDistribution
+    p_ϕ::UnivariateDistribution
+    # Line
+    p_a::UnivariateDistribution
+    p_b::UnivariateDistribution
+    # Marginal
+    p_x::UnivariateDistribution
+    # Error
+    p_ϵ::UnivariateDistribution
+end
+function SinusoidLinearMixture()
+    return SinusoidLinearMixture(Bernoulli(),
+                                 Uniform(0.1, 0.5),
+                                 Uniform(0.0, π),
+                                 Uniform(-3.0, 3.0),
+                                 Uniform(-3.0, 3.0),
+                                 Uniform(-5.0, 5.0),
+                                 Normal(0.0, 0.3^2))
+end
 
+function Base.rand(p::SinusoidLinearMixture, k::Int)
+    X = rand(p.p_x, k)
+    if rand(p.p_sinusoid)
+        A = rand(p.p_A)
+        ϕ = rand(p.p_ϕ)
+        Y = @. A * sin(X + ϕ)
+    else
+        a = rand(p.p_a)
+        b = rand(p.p_b)
+        Y = @. a * X + b
+    end
+    return X, Y
+end
+function Base.rand(p::SinusoidLinearMixture)
+    x = rand(p.p_x)
+    if rand(p.p_sinusoid)
+        A = rand(p.p_A)
+        ϕ = rand(p.p_ϕ)
+        y = A * sin(X + ϕ)
+    else
+        a = rand(p.p_a)
+        b = rand(p.p_b)
+        y = a * X + b
+    end
+    return x, y
+end
 
 # Uncertainty in Multitask Transfer learning, Harmonics
 # NOTE: Unclear what sigma_y is from paper
@@ -40,7 +87,6 @@ struct Harmonic <: AbstractSyntheticDataSet
     p_meta_x::UnivariateDistribution
     p_ϵ::UnivariateDistribution
 end
-
 Harmonic() = Harmonic(Uniform(0.0, 1.0), Uniform(5.0, 7.0), Uniform(0, 2π), Uniform(-4.0, 4.0), Normal(0.0, 0.1))
 
 function Base.rand(p::Harmonic, k::Int)
@@ -69,21 +115,3 @@ function Base.rand(p::Harmonic)
     y = A₁ * sin(ω * x + ϕ₁) + A₂ * sin(2ω * x + ϕ₂) + ϵ
     return x, y
 end
-
-using Plots; pyplot()
-maml_sine = Sinusoid()
-harmonic = Harmonic()
-
-X, Y = rand(harmonic, 500)
-sortind = sortperm(X)
-X = X[sortind]
-Y = Y[sortind]
-p = plot(X, Y)
-for i in 1:5
-    X, Y = rand(harmonic, 500)
-    sortind = sortperm(X)
-    X = X[sortind]
-    Y = Y[sortind]
-    plot!(p, X, Y)
-end
-p
